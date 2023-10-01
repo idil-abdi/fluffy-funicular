89004f7f9657f40b93ecc1bfb0d95b24e2bf7212
Edil Abdiaziz <edilabdiaziz@gmail.com>
2023-10-01 13:00:54 +0100

    added a flip feature

--- asset/JavaScript/index.js
+++ asset/JavaScript/index.js
@@ -44,15 +44,39 @@ const constructGame = (length) => {
     const stitchLocation = getRandomCell(length);
 
     for (let row = 0; row < length; row++) {
-        const rowDiv =document.createElement('div');
+        const rowDiv = document.createElement('div');
         rowDiv.setAttribute('class', 'row');
 
         for (let col = 0; col < 3; col++) {
             const colDiv =document.createElement('div');
             colDiv.setAttribute('class', 'col');
-            colDiv.setAttribute("data-row", row);
-            colDiv.setAttribute("data-col", col);
-            colDiv.textContent = '?'
+
+            for(let card = 0; card < 1; card++ ) {
+                const cardDiv = document.createElement('div');
+                cardDiv.setAttribute('class', 'flip-card');
+
+                for(let i = 0; i < 1; i++) {
+                    const innerDiv = document.createElement('div');
+                    innerDiv.setAttribute('class', 'inner');
+
+                    for(let sub = 0; sub < 1; sub++) {
+                        const frontDiv = document.createElement('div');
+                        frontDiv.setAttribute('class', 'front');
+                        frontDiv.setAttribute("data-row", row);
+                        frontDiv.setAttribute("data-col", col);
+                        frontDiv.textContent = '?';
+                        const backDiv = document.createElement('div');
+                        backDiv.setAttribute('class', 'back');
+
+                        innerDiv.append(...[frontDiv, backDiv]);
+                    }
+
+                    cardDiv.append(innerDiv);
+                };
+
+                colDiv.append(cardDiv);
+            };
+
             rowDiv.append(colDiv);
         };
 
@@ -73,13 +97,13 @@ const handleCellClick = (event, stitchLocation) => {
     const selectedCol = +target.getAttribute("data-col");
 
     if (selectedRow === stitchLocation.row && selectedCol === stitchLocation.col) {
-        console.log('Found Stitch');
+        console.log('FOUND STITCH');
     } else {
         console.log("BOO");
     }
 };
 
-const getRandomCell =(length) => {
+const getRandomCell = (length) => {
     return {
         row: Math.floor(Math.random() * length),
         col: Math.floor(Math.random() * length),
